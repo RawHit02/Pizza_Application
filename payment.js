71e294f58ae4040c33468e8b6aa1a103786f7d02
Rohit Panwar <107709247+RawHit02@users.noreply.github.com>
2023-09-01 01:22:47 +0530

    Update payment.js

--- payment.js
+++ payment.js
@@ -4,7 +4,7 @@ var options = {
     "currency": "INR",
     "name": "Pizza Shop", //your business name
     "description": "Pizza Shop Transaction",
-    "image": "https://example.com/your_logo",
+    "image": "https://imgs.search.brave.com/YVbAkVFK6R9scg7TKwDjfTCS15GmoNxwbXKYwKi_h9A/rs:fit:860:0:0/g:ce/aHR0cDovL2dldGRy/YXdpbmdzLmNvbS9m/cmVlLWljb24tYncv/YmlsbC1vZi1tYXRl/cmlhbC1pY29uLTEu/cG5n",
     
     "handler": function (response){
         alert("Payment Done....");
