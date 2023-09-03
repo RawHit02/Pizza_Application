a6a59c3c147b937574007475824f7adc0054c129
Rohit Panwar <107709247+RawHit02@users.noreply.github.com>
2023-09-03 10:05:12 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,1 @@
+export const URL = 'https://raw.githubusercontent.com/RawHit02/MERN_Stack_JS/main/server.json';
---
+++ payment.js
@@ -0,0 +1,41 @@
+var options = {
+    "key": "rzp_test_9TO14DgPoIsohI", // Enter the Key ID generated from the Dashboard
+    "amount": "50000", // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
+    "currency": "INR",
+    "name": "Pizza Shop", //your business name
+    "description": "Pizza Shop Transaction",
+    "image": "https://imgs.search.brave.com/YVbAkVFK6R9scg7TKwDjfTCS15GmoNxwbXKYwKi_h9A/rs:fit:860:0:0/g:ce/aHR0cDovL2dldGRy/YXdpbmdzLmNvbS9m/cmVlLWljb24tYncv/YmlsbC1vZi1tYXRl/cmlhbC1pY29uLTEu/cG5n",
+    
+    "handler": function (response){
+        alert("Payment Done....");
+        alert(response.razorpay_payment_id);
+        alert(response.razorpay_order_id);
+        alert(response.razorpay_signature)
+    },
+    "prefill": { //We recommend using the prefill parameter to auto-fill customer's contact information, especially their phone number
+        "name": "Customer Name", //your customer's name
+        "email": "customerEmail@example.com", 
+        "contact": "CustomerPhone"  //Provide the customer's phone number for better conversion rates 
+    },
+    "notes": {
+        "address": "Razorpay Corporate Office"
+    },
+    "theme": {
+        "color": "#3399cc"
+    }
+};
+var rzp1 = new Razorpay(options);
+rzp1.on('payment.failed', function (response){
+    alert("Payment Fail");    
+    alert(response.error.code);
+        alert(response.error.description);
+        alert(response.error.source);
+        alert(response.error.step);
+        alert(response.error.reason);
+        alert(response.error.metadata.order_id);
+        alert(response.error.metadata.payment_id);
+});
+document.getElementById('rzp-button1').addEventListener('click', function(){
+    rzp1.open();
+    e.preventDefault();
+});
